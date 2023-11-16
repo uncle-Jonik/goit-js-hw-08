8cae06cbe98bc86c14a7addeb1ee34da2239e4bf
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-16 22:51:22 +0200

    add 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Change code below this line
 console.log(galleryItems);
+
+
+const ulContainer = document.querySelector('.gallery')
+
+ulContainer.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+function createMarkup(arr) {
+    return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>
+            </li>
+    `
+    )
+    .join('');
+}
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
