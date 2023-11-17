8a193bdd1311f9146a323ed58071f515f951e4c6
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-17 02:07:12 +0200

    add 2nd task
    not completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function currentVideoTime() {
+  player.on('timeupdate', function () {
+    let value = 0;
+    value += event.data.data.seconds;
+
+    localStorage.setItem('videoplayer-current-time', value);
+  });
+}
+
+currentVideoTime();
+
+const currentBreakpoint = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentBreakpoint);
