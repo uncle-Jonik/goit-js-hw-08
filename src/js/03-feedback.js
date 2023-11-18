082e19d10f5b6f4cd74a7e31e5eb361b2e2ada8e
uncle-Jonik <Katsimon_Eu@outlook.com>
2023-11-18 14:40:33 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onInputListener);
+
+function onInputListener(evt) {
+  const email = evt.currentTarget.elements.email.value;
+  const message = evt.currentTarget.elements.message.value;
+  const data = {
+    email,
+    message,
+  };
+  console.log(data);
+}
+
+// console.dir(form);
+// console.log(form.children[0]);
+// const settings = {
+//   theme: 'dark',
+//   isAuthenticated: true,
+//   options: [1, 2, 3],
+// };
+// localStorage.setItem('settings', JSON.stringify(settings));
+
+// const savedSettings = localStorage.getItem('settings');
+// const parsedSettings = JSON.parse(savedSettings);
+// console.log(parsedSettings); // settings object
+
+// const localStorageKey = 'feedback-form-state';
+
+// form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
+
+// throttle(function () {
+//   form.addEventListener(
+//     'input',
+//     evt => {
+//       localStorage.setItem(localStorageKey, evt.target.value);
+//     },
+//     500
+//   );
+// });
+
+// form.addEventListener('submit', evt => {
+//   evt.preventDefault();
+//   localStorage.removeItem(localStorageKey);
+//   form.reset();
+// });
