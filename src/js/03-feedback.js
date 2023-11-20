a1502c7e8bbff08fa95f2d803293eae3be8afa66
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2023-11-20 17:04:27 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,41 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
-
-form.addEventListener('input', onInputListener);
-
-function onInputListener(evt) {
-  const email = evt.currentTarget.elements.email.value;
-  const message = evt.currentTarget.elements.message.value;
-  const data = {
-    email,
-    message,
-  };
-  console.log(data);
+const localStorageKey = 'feedback-form-state';
+//
+//
+function chekInputValue(key) {
+  // перевірка чи заповненні поля email та message
+  const savedData = localStorage.getItem(key);
+  const parsedData = JSON.parse(savedData);
+  if (parsedData === null) {
+    return;
+  }
+  form[0].value = parsedData.email;
+  form[1].value = parsedData.message;
 }
+chekInputValue(localStorageKey);
+//
+//
+form.addEventListener('input', throttle(onInputListener, 500));
+function onInputListener(evt) {
+  // створення та запис данних до локального сховища
+  const enteredData = {
+    email: evt.currentTarget[0].value,
+    message: evt.currentTarget[1].value,
+  };
 
-// console.dir(form);
-// console.log(form.children[0]);
-// const settings = {
-//   theme: 'dark',
-//   isAuthenticated: true,
-//   options: [1, 2, 3],
-// };
-// localStorage.setItem('settings', JSON.stringify(settings));
-
-// const savedSettings = localStorage.getItem('settings');
-// const parsedSettings = JSON.parse(savedSettings);
-// console.log(parsedSettings); // settings object
-
-// const localStorageKey = 'feedback-form-state';
-
-// form.elements.message.value = localStorage.getItem(localStorageKey) ?? '';
-
-// throttle(function () {
-//   form.addEventListener(
-//     'input',
-//     evt => {
-//       localStorage.setItem(localStorageKey, evt.target.value);
-//     },
-//     500
-//   );
-// });
-
-// form.addEventListener('submit', evt => {
-//   evt.preventDefault();
-//   localStorage.removeItem(localStorageKey);
-//   form.reset();
-// });
+  localStorage.setItem(localStorageKey, JSON.stringify(enteredData));
+  const savedSettings = localStorage.getItem(localStorageKey);
+  const parsedSettings = JSON.parse(savedSettings);
+  console.log(parsedSettings);
+}
+//
+//
+form.addEventListener('submit', onSubmitListener);
+function onSubmitListener(evt) {
+  //очистка сховища та даних в полях
+  evt.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+}
